5c336d81213f711bc42ae6b1d7a541d0452d2fe4
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-10-14 00:37:13 +0300

    Fix validateInput function

--- form.js
+++ form.js
@@ -1,41 +1,26 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-    validateInputs();
-    
-});
-
 const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
-}
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
 };
 
-
-
 const validateInputs = () => {
-const emailValue = email.value.trim();
+  const mailformat = /^[a-z0-9_!#$%&'*+=?`{|}~^.-]+@[a-z0-9.-]+$/gm;
+  if (email.value.match(mailformat)) {
+    form.submit();
+  } else {
+    setError(email, 'The form is not sent, Email accept lowerCase only');
+  }
+};
 
-if(emailValue === '') {
-  setError(email, 'Email is required');
-} else if (emailValue === emailValue.toUpperCase()) {
-  setError(email, 'The form is not send, Email accept lowerCase only');
-} else {
-  form.submit();
-}
-}
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  validateInputs();
+});
